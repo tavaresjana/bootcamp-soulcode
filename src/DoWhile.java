1792257d5efe4381644720ec03a7ee34f75e487c
Janaina Tavares <78567230+tavaresjana@users.noreply.github.com>
2024-03-14 09:56:52 -0300

    feat: aula de arrays

---
+++ src/DoWhile.java
@@ -0,0 +1,13 @@
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int numero =-5;
+
+        do{
+            System.out.println("1 - somar");
+            System.out.println("1 - dividir");
+            System.out.println("-1 - sair");
+
+        } while (numero >= 0);
+    }
+}
